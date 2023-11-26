0ecbbd10d0e96ce0baf25907d538a80b3c8b9d54
Htoo Aung Lin <htooaunglin@yomabank.com>
2023-11-26 20:09:51 +0630

    updated

--- ProfileScreen/Profile.js
+++ ProfileScreen/Profile.js
@@ -1,27 +1,41 @@
+import React from "react";
+import {View,Text,Image,StyleSheet} from 'react-native';
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-const Profile = () => {
-  return (
+const Profile=()=>{
+  return(
     <View style={styles.container}>
-      <Text style={styles.title}>Profile Screen</Text>
-      
+      <View style={styles.imageContainer}>
+        <Image source={require('../assets/images/logo.png')} style={styles.image}/>
+        <Text style={styles.idText}>Employee ID:010311</Text>
+      </View>
     </View>
   );
 };
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    padding:16,
+  },
+  imageContainer:{
+    alignItems:'center',
+  },
+  image:{
+    width:100,
+    height:100,
+    borderRadius:50,
+    marginBottom:10,
+  },
+  idText:{
+    fontSize:16,
+    fontWeight:'bold',
+  },
+  title:{
+    fontSize:24,
+    fontWeight:'bold',
+  },
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
+  
 });
-
 export default Profile;
